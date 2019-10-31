fa31dad16c2605f63c28a7a84f34887e08f78bcf
Kyle Payne <kspayne93@gmail.com>
2019-10-30 20:34:48 -0500

    added onClick to logo button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -19,7 +19,11 @@ class Sidebar extends Component {
 
       return (
          <div className='sidebar'>
-            <Link to='/dashboard' style={{ textDecoration: 'none' }}>
+            <Link
+               to='/dashboard'
+               style={{ textDecoration: 'none' }}
+               onClick={() => this.setState({ selectedButton: 'overview' })}
+            >
                <div className='sidebar-logo-container logo-text'>
                   <img src={NimbleLogo} alt="Nimble Logo"/>
                   <p>NIMBLE</p>
@@ -33,10 +37,10 @@ class Sidebar extends Component {
                   style={{ textDecoration: 'none' }}
                >
                   <Tooltip title={'All Project Tasks'} enterDelay={300}>
-                           <div className='sidebar-button'>
-                              <i className="fas fa-th-large"></i>
-                              <p>OVERVIEW</p>
-                           </div>
+                     <div className='sidebar-button'>
+                        <i className="fas fa-th-large"></i>
+                        <p>OVERVIEW</p>
+                     </div>
                   </Tooltip>
                </Link>
                <Tooltip title={'Only My Tasks'} enterDelay={300}>
@@ -44,8 +48,8 @@ class Sidebar extends Component {
                      onClick={() => this.handleButtonClick('my-tasks')}
                      className={selectedButton === 'my-tasks' ? 'sidebar-button active-button' : 'sidebar-button'}
                   >
-                        <i class="far fa-user"></i>
-                        <p>MY TASKS</p>
+                     <i class="far fa-user"></i>
+                     <p>MY TASKS</p>
                   </div>
                </Tooltip>
                <Tooltip title={'All Uncompleted Tasks'} enterDelay={300}>
@@ -53,8 +57,8 @@ class Sidebar extends Component {
                      onClick={() => this.handleButtonClick('todo')}
                      className={selectedButton === 'todo' ? 'sidebar-button active-button' : 'sidebar-button'}
                   >
-                        <i className="fas fa-tasks"></i>
-                        <p>TO DO</p>
+                     <i className="fas fa-tasks"></i>
+                     <p>TO DO</p>
                   </div>
                </Tooltip>
                <Tooltip title={'All Tasks In Progress'} enterDelay={300}>
@@ -62,8 +66,8 @@ class Sidebar extends Component {
                      onClick={() => this.handleButtonClick('in-progress')}
                      className={selectedButton === 'in-progress' ? 'sidebar-button active-button' : 'sidebar-button'}
                   >
-                        <i className="fas fa-spinner"></i>
-                        <p>IN PROGRESS</p>
+                     <i className="fas fa-spinner"></i>
+                     <p>IN PROGRESS</p>
                   </div>
                </Tooltip>
                <Tooltip title={'All Completed Tasks'} enterDelay={300}>
@@ -71,8 +75,8 @@ class Sidebar extends Component {
                      onClick={() => this.handleButtonClick('completed')}
                      className={selectedButton === 'completed' ? 'sidebar-button active-button' : 'sidebar-button'}
                   >
-                        <i className="far fa-check-square"></i>
-                        <p>COMPLETED</p>
+                     <i className="far fa-check-square"></i>
+                     <p>COMPLETED</p>
                   </div>
                </Tooltip>
             </div>
