ec50755022f3472d9c30027d86cabff320ad42a2
Kyle Payne <kspayne93@gmail.com>
2019-11-09 12:24:48 -0700

    added seed data for database table creation

--- database/seed.sql
+++ database/seed.sql
@@ -1,1 +1,61 @@
 -- Initialize db tables
+
+CREATE TABLE user (
+	id SERIAL PRIMARY KEY,
+	first_name character varying(50) NOT NULL,
+	last_name character varying(50) NOT NULL,
+	email character varying(50) NOT NULL,
+	password character varying(256) NOT NULL
+);
+
+CREATE TABLE project (
+	id SERIAL PRIMARY KEY,
+	name character varying(50) NOT NULL,
+	created_at timestamp without time zone NOT NULL,
+	archived boolean NOT NULL
+);
+
+CREATE TABLE lists (
+	id SERIAL PRIMARY KEY,
+	name character varying(50) NOT NULL,
+	color_code character varying(10) NOT NULL,
+	archived boolean NOT NULL,
+	project_id integer NOT NULL
+);
+
+CREATE TABLE task (
+	id SERIAL PRIMARY KEY,
+	name character varying(50) NOT NULL,
+	status character varying(50) NOT NULL
+);
+
+CREATE TABLE project_users (
+	id SERIAL PRIMARY KEY,
+	project_id integer NOT NULL REFERENCES project(id),
+	user_id integer NOT NULL REFERENCES user(id)
+);
+
+CREATE TABLE project_lists (
+	id SERIAL PRIMARY KEY,
+	project_id integer NOT NULL REFERENCES project(id),
+	list_id integer NOT NULL REFERENCES list(id)
+);
+
+CREATE TABLE list_tasks (
+	id SERIAL PRIMARY KEY,
+	list_id integer NOT NULL REFERENCES list(id),
+	task_id integer NOT NULL REFERENCES task(id)
+);
+
+CREATE TABLE task_users (
+	id SERIAL PRIMARY KEY,
+	task_id integer NOT NULL REFERENCES task(id),
+	user_id integer NOT NULL REFERENCES user(id)
+);
+
+CREATE TABLE checklist_item (
+	id SERIAL PRIMARY KEY,
+	name character varying(50) NOT NULL,
+	completed boolean NOT NULL,
+	list_id integer NOT NULL REFERENCES list(id)
+);
