f0e774b896695e5c46a231f827978fdf822a4a91
Kyle Payne <kspayne93@gmail.com>
2019-12-12 23:13:23 -0700

    added

---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { SwatchesPicker } from 'react-color';
+
+export default class ColorPicker extends Component {
+	render() {
+		return (
+			<div
+				style={{
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'flexStart',
+					alignItems: 'center'
+				}}
+				onClick={this.props.closeColorPicker}
+			>
+				<div onClick={event => event.stopPropagation()}>
+					<SwatchesPicker onChange={e => this.props.handleColorChange(e)}/>
+				</div>
+			</div>
+		)
+	}
+}
