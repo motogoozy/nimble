95089598633c5496c0c493b0a8b8c4edd3f2e7dd
Kyle Payne <kspayne93@gmail.com>
2019-12-19 16:35:11 -0700

    added task order column

--- db/list/create_list.sql
+++ db/list/create_list.sql
@@ -1,4 +1,4 @@
-INSERT INTO list (title, color_code, archived, project_id)
-VALUES (${title}, ${color_code}, ${archived}, ${project_id})
+INSERT INTO list (title, color_code, archived, project_id, task_order)
+VALUES (${title}, ${color_code}, ${archived}, ${project_id}, ${task_order})
 
 RETURNING *;
