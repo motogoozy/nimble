5e104ab9225bbe67d6d4253230d69a6e66c29e64
Kyle Payne <kspayne93@gmail.com>
2019-12-24 11:18:39 -0800

    updated query

--- db/task/get_tasks_by_user_id.sql
+++ db/task/get_tasks_by_user_id.sql
@@ -1,4 +1,4 @@
-SELECT title
+SELECT t.task_id, t.title, t.list_id, t.project_id, t.created_at, t.created_by, t.status, u.user_id
 FROM task t
 	JOIN task_users tu ON tu.task_id = t.task_id
 	JOIN users u ON u.user_id = tu.user_id
