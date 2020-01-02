0a035a07848cc963655252baa87df995ef34514d
Kyle Payne <kspayne93@gmail.com>
2020-01-02 16:15:09 -0700

    added component

---
+++ src/components/People/People.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './People.scss';
+
+export default class PeoplePage extends Component {
+	render() {
+		return (
+			<div className='people-page'>
+				<h1>People Component</h1>
+			</div>
+		)
+	}
+}
