499e5f0d9d9205e72148215571cab3d8579f5ebb
Kyle Payne <kspayne93@gmail.com>
2020-02-25 15:19:05 -0700

    simplified

--- src/components/People/UserConnection/UserConnection.jsx
+++ src/components/People/UserConnection/UserConnection.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import './UserConnection.scss';
 import Avatar from '../../Avatar/Avatar';
 
-import Tooltip from '@material-ui/core/Tooltip';
-
 export default function UserConnection(props) {
-   const { user, actions, tooltipTitles, avatarColor } = props;
+   const { user, avatarColor } = props;
    const firstInitial = user.first_name.split('')[0];
    const lastInitial = user.last_name.split('')[0];
    const userInitials = firstInitial + lastInitial;
@@ -20,17 +18,6 @@ export default function UserConnection(props) {
                <p>{user.first_name} {user.last_name}</p>
                <p>{user.email}</p>
             </div>
-         </div>
-         <div className='connection-button-container'>
-            {
-               actions.map((action, idx) => {
-                  return (
-                     <Tooltip title={tooltipTitles[idx]} key={tooltipTitles[idx]+idx}>
-                        <p className='cursor-pointer'>{action}</p>
-                     </Tooltip>
-                  )
-               })
-            }
          </div>
       </div>
    )
