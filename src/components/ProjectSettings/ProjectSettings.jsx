af93275a9137615a10eda9f9065bdb48f4ad7ea6
Kyle Payne <kspayne93@gmail.com>
2020-03-06 13:43:04 -0700

    changed from class to functional compoent with hooks, styled

--- src/components/ProjectSettings/ProjectSettings.jsx
+++ src/components/ProjectSettings/ProjectSettings.jsx
@@ -1,56 +1,99 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProjectSettings.scss';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Tooltip from '@material-ui/core/Tooltip';
 import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
 
-export default class ProjectSettings extends Component {
-   state = {
-      projectTitle: '',
+export default function ProjectSettings(props) {
+   const [projectTitle, setProjectTitle] = useState('');
+   const [permissions, setPermissions] = useState({
+      editLists: false,
+      deleteLists: false,
+      editTasks: false,
+      deleteTasks: false,
+   });
+
+   useEffect(() => setProjectTitle(props.project.title), [props.project.title]);
+   useEffect(() => console.log(permissions), [permissions]);
+
+
+   const handleSwitch = (key, event) => {
+      setPermissions({ ...permissions, [key]: event.target.checked })
    };
 
-   componentDidMount = async () => {
-      this.setState({
-         projectTitle: this.props.project.title
-      })
-   };
+   const CustomSwitch = withStyles({
+      switchBase: {
+      color: '#663f56',
+      '&$checked': {
+         color: '#995D81',
+      },
+      '&$checked + $track': {
+         backgroundColor: '#995D81',
+      },
+      },
+      checked: {},
+      track: {},
+   })(Switch);
 
-   handleInput = (key, value) => {
-      this.setState({ [key]: value });
-   };
-
-   render() {
-      const { projectTitle } = this.state;
-      const { project } = this.props;
-      return (
-         <div className='project-settings-main'>
-            <div className='project-settings-header'>
-               <p>Project Settings - {project.title}</p>
+   return (
+      <div className='project-settings-main'>
+         <div className='project-settings-header'>
+            <p style={{ fontWeight: '500' }}>Project Settings - {projectTitle}</p>
+         </div>
+         <div className='project-settings-body'>
+            <p style={{ fontWeight: '400' }}>Project Title:</p>
+            <div className='project-setting project-title-input-container'>
+               <TextField
+                  required
+                  placeholder='Title (60 chars max)'
+                  id="standard-required"
+                  value={projectTitle}
+                  onChange={e => setProjectTitle(e.target.value)}
+               />
             </div>
-            <div className='project-settings-body'>
-               <div className='project-settings-container'>
-                  <div className='project-setting-container'>
-                     <p>Project Title</p>
-                     <TextField
-								required
-								placeholder='Title (60 chars max)'
-                        id="standard-required"
-                        value={projectTitle}
-                        onChange={e => this.handleInput('projectTitle', e.target.value)}
-                     />
-                  </div>
-                  <Tooltip title={'Delete List'}>
-                     <div className='delete-project-container cursor-pointer'>
-                        <IconButton aria-label="delete">
-                           <DeleteIcon />
-                        </IconButton>
-                        <p>Delete Project</p>
-                     </div>
-                  </Tooltip>
+            <div className='project-permissions-container'>
+               <p style={{ fontWeight: '400' }}>Allow Collaborators to:</p>
+               <div className='project-setting permission-switch'>
+                  <p>Edit Lists</p>
+                  <CustomSwitch
+                     checked={permissions.editLists}
+                     onChange={event => handleSwitch('editLists', event)}
+                  />
+               </div>
+               <div className='project-setting permission-switch'>
+                  <p>Delete Lists</p>
+                  <CustomSwitch
+                     checked={permissions.deleteLists}
+                     onChange={event => handleSwitch('deleteLists', event)}
+                  />
+               </div>
+               <div className='project-setting permission-switch'>
+                  <p>Edit Tasks</p>
+                  <CustomSwitch
+                     checked={permissions.editTasks}
+                     onChange={event => handleSwitch('editTasks', event)}
+                  />
+               </div>
+               <div className='project-setting permission-switch'>
+                  <p>Delete Tasks</p>
+                  <CustomSwitch
+                     checked={permissions.deleteTasks}
+                     onChange={event => handleSwitch('deleteTasks', event)}
+                  />
                </div>
             </div>
+            <Tooltip title={'Delete List'}>
+               <div className='delete-project-container cursor-pointer'>
+                  <IconButton aria-label="delete">
+                     <DeleteIcon />
+                  </IconButton>
+                  <p>Delete Project</p>
+               </div>
+            </Tooltip>
          </div>
-      )
-   }
+      </div>
+   )
 }
