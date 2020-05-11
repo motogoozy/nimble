e5ead02316737f38e679e41ff33817c9073c1461
Kyle Payne <kspayne93@gmail.com>
2020-05-11 10:31:58 -0600

    added add_collaborators & remove_collaborators

--- db/project/update_project_permissions.sql
+++ db/project/update_project_permissions.sql
@@ -6,6 +6,8 @@ SET
 	add_lists = ${add_lists},
 	edit_lists = ${edit_lists},
 	delete_lists = ${delete_lists},
-	edit_project = ${edit_project}
+	edit_project = ${edit_project},
+	add_collaborators = ${add_collaborators},
+	remove_collaborators = ${remove_collaborators}
 WHERE project_id = ${project_id}
 RETURNING *;
