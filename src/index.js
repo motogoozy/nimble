21b2315a3e6c16a587585e72e8178fc2e644c90a
Kyle Payne <kspayne93@gmail.com>
2020-05-29 11:56:27 -0600

    added normalize.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import 'normalize.css';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
