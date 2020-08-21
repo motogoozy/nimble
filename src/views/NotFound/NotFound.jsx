6449a832152974a6172cf1cf8e2f2e0e80d3dfc0
Kyle Payne <kspayne93@gmail.com>
2020-08-21 11:57:53 -0600

    change font to sans-serif

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFound = () => (
-  <div style={{ textAlign: 'left', margin: '1rem' }}>
+  <div style={{ textAlign: 'left', margin: '1rem', fontFamily: 'sans-serif' }}>
     <h2>404 page not found</h2>
     <p>The page you are looking for does not exist.</p>
   </div>
