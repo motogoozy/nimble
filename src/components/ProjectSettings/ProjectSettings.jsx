99f50c80e8830c29cf9fe5daa2bb203380a7d0b6
Kyle Payne <kspayne93@gmail.com>
2020-11-30 13:34:07 -0700

    remove unnecessary import

--- src/components/ProjectSettings/ProjectSettings.jsx
+++ src/components/ProjectSettings/ProjectSettings.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './ProjectSettings.scss';
 import GlobalContext from '../../GlobalContext';
 import useProjectTitle from '../../hooks/useProjectTitle';
